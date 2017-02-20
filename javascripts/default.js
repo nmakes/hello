78833ae8c077eaae6da6210cd194c9a7345d7dd7
nmakes <nav.naveenvenkat@gmail.com>
2017-02-20 01:16:59 +0000

    added onblur for searchbar

--- javascripts/default.js
+++ javascripts/default.js
@@ -7,3 +7,12 @@ function confirmtest(msg)
 {
 	return confirm(msg);
 }
+
+function search_focus_script(){
+	document.getElementById("search_bar").placeholder="search for 'what is life?'";
+}
+
+function search_out_script(){
+
+	document.getElementById("search_bar").placeholder="what is life?";
+}
